018e645281f1a4b0af36a23c750ded7ae5b21ac2
Iris Beckham <irisdancer3@gmail.com>
2023-05-24 12:54:49 -0400

    initial setup of simple functions

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const wordsData = require("./words.json");
 
-console.log(wordsData);
+/** logs all the words.
+ * @param {string[]} words
+*/
+function allWords(words){}
+
+/**logs the first ten words.
+ * @param {string[]} words
+*/
+function firstTenWords(words){}
+
+/**logs the next 10 words.
+ * @param {string[]} words
+*/
+function nextTenWords(words, index){}
+
+/**logs from 0 to x words.
+ * @param {string[]} words
+*/
+function firstXWords(words, index){}
+
+/**logs from x to y words.
+ * @param {string[]} words
+*/
+function subsetOfWords(words, firstIndex, lastIndex){}
+
+/**logs all the words, sorted alphabetically.
+ * @param {string[]} words
+*/
+function sortWords(words){}
