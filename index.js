4e6191f9d4b75a9a803a37872050c8ff2b29ae27
Kenneth-Parker <kennethkavon@gmail.com>
2023-05-25 16:09:18 -0400

    index.js, package .json, basic function initialization

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const words = require("./words.json")
+// console.log(words)
+
+// `allWords` logs all the words.
+const allWords = (words) => words
+
+console.log(allWords(words))
+
+// `firstTenWords` logs the first ten words.
+
+// firstTenWords(words)
+
+//  `nextTenWords` logs the next 10 words.
+
+//  `firstXWords` logs from 0 to x words.
+
+//  `subsetOfWords` logs from x to y words.
+
+//  `sortWords` logs all the words, sorted alphabetically.
+
