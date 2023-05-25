ecf2de1bf93047e8324e6616f15c5baad0f6f4b6
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-05-25 01:11:06 -0400

    still working for lettersExactMatch() & main()

--- index.js
+++ index.js
@@ -17,10 +17,17 @@ function allWords(words = wordsJson){
  * firstTenWords()
  * -----------------------
  * showing first ten words (from index 0 to 9)
+ * if the size of [words] less than 10, return -1 w/ error log.
  * 
  * @param {string[]} words - A data set for words, default value is JSON file, [wordsJson]
  */
 function firstTenWords(words = wordsJson){
+    /** validation for size of the data */
+    if(words.length < 10){
+        console.log("Our data set does have less than 10 words");
+        return -1;
+    }
+
     for(let index = 0; index < 10; index++){
         console.log(words[index]);
     }
@@ -32,9 +39,14 @@ function firstTenWords(words = wordsJson){
  * showing 10 words which begin w/ index [position]
  * 
  * @param {string[]} words - A data set for words, default value is JSON file, [wordsJson]
- * @param {number} position 
+ * @param {number} position - a start position
  */
 function nextTenWords(words = wordsJson, position = 0){
+    /** validation for size of the data */
+    if(words.length < 10){
+        console.log("Our data set does have less than 10 words");
+        return -1;
+    }
     for(let index = position; index < position + 10; index ++){
         console.log(words[index]);
     }
@@ -46,9 +58,13 @@ function nextTenWords(words = wordsJson, position = 0){
  * showing words from index 0 to index [end]
  * 
  * @param {string[]} words - A dataset for words, deafult value is JSON file, [wordsJson] 
- * @param {number} end - the end position for words
+ * @param {number} end - an end position for words
  */
 function firstXWords(words = wordsJson, end = words.length){
+    if(end > words.length){
+        console.log("Out of range");
+        return -1;
+    }
     for(let index = 0; index < end; index++){
         console.log(words[index]);
     }
@@ -64,6 +80,17 @@ function firstXWords(words = wordsJson, end = words.length){
  * @param {*} end - the end position for words
  */
 function subsetOfWords(words = wordsJson, start = 0, end = words.length){
+    /** validation */
+    if(end > words.length || start >= words.length){
+        console.log("Out of range");
+        return -1;
+    }
+
+    if(start > end){
+        console.log("the value of the start position must be less than the value of the end position");
+        return -1;
+    }
+
     for(let index = start; index < end; index++){
         console.log(words[index]);
     }
@@ -99,23 +126,27 @@ function wordsWithQ(words = wordsJson){
  * 
  * @param {string[]} words - A dataset for words, deafult value is JSON file, [wordsJson] 
  * @param {string} letter - A character to filter in 
- * @returns 
+ * @returns {string[]} - a filtered array of strings
  */
 function findWordsWithLetter(words = wordsJson, letter){
     return words.filter(a => a.includes(letter));
 }
 
-
-
-
+/**
+ * lettersMatch()
+ * -----------------------------
+ * return any words that contain [letters] in any order
+ * 
+ * @param {string[]} words - A dataset for words, deafult value is JSON file, [wordsJson] 
+ * @param {string} letters - A string, up to 5 letters, to filter in [words] 
+ * @returns {string[]} - a filtered array of strings
+ */
 function lettersMatch(words = wordsJson, letters){
-    let temp;
+    let temp = words;
+    letters = letters.split('');
     for(let index = 0; index < letters.length; index++){
-        console.log(letters[index]);
         if(letters[index] != "." || letters[index] != "_"){
-            console.log("we are here!")
-            temp = words.filter(a => a[index] == letters[index]);
-            console.log(temp)
+            temp = findWordsWithLetter(temp, letters[index]);
         }
     }
 
@@ -126,8 +157,8 @@ function main(){
     console.log("HELLO");
 }
 
-//console.log(lettersMatch(wordsJson, "qophs"));
-console.log(allWords())
+console.log(lettersMatch(wordsJson, "qop"));
+
 module.exports = {
     allWords,
     firstTenWords,
