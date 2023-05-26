9163d77fc7928e94841eb0ac0b8699e163ca28f8
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-05-26 00:30:32 -0400

    4 simple functions

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const wordsArr = require("./words.json")
 const allWords = words => words
 console.log(allWords(wordsArr))
 
-const firstTenWords = words => {
-    let firstTen = []
-    for (let i = 0; i < words.length; i++){
-        if (i < 10){
-            firstTen.push(words[i])
-        }
-    }
-    return firstTen
-}
+const firstTenWords = words => words.slice(0, 10)
 console.log(firstTenWords(wordsArr))
+
+const nextTenWords = words => words.slice(10, 20)
+console.log(nextTenWords(wordsArr))
+
+const firstXWords = (words, x) => words.slice(0, x)
+console.log(firstXWords(wordsArr))
