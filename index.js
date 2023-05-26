5f4881ecbae326ad2929ed2eb8297f1e6422ad26
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-05-26 01:39:44 -0400

    all functions

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const wordsArr = require("./words.json")
 
 const allWords = words => words
-console.log(allWords(wordsArr))
 
 const firstTenWords = words => words.slice(0, 10)
-console.log(firstTenWords(wordsArr))
 
 const nextTenWords = words => words.slice(10, 20)
-console.log(nextTenWords(wordsArr))
 
 const firstXWords = (words, x) => words.slice(0, x)
-console.log(firstXWords(wordsArr))
+
+const subsetOfWords = (words, x, y) => words.slice(x, y)
+
+const sortWords = words => words.sort()
+
+const wordsWithQ = words => words.filter(word => word.includes("q"))
+
+const letter = process.argv[2]
+const findWordsWithLetter = (words, letter) => words.filter(word => word.includes(letter))
+const findWords = findWordsWithLetter(wordsArr, letter)
+console.log(`Words with the letter '${letter}': ${findWords}`)
