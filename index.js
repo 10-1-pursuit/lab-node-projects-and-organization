2cde1853fb2535023ec9d1dc46fdd7fdeff22b7f
Kenneth-Parker <kennethkavon@gmail.com>
2023-05-25 23:05:30 -0400

    Start Simple complete✅✅

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 
 
+// ### Start simple
+
 const words = require("./words.json")
 // console.log(words)
 
 // `allWords` logs all the words.
 const allWords = (words) => words
-console.log(allWords(words))
+// console.log(allWords(words))
 
 // `firstTenWords` logs the first ten words.
-const firstTenWords = (words) => words.slice(0,9)
-console.log(firstTenWords(words))
+const firstTenWords = (words) => words.slice(0,10)
+// console.log(firstTenWords(words))
 
 //  `nextTenWords` logs the next 10 words.
-const nextTenWords = (words) => words.slice(9,19)
-console.log(nextTenWords(words))
+const nextTenWords = (words) => words.slice(10,20)
+// console.log(nextTenWords(words))
 
 //  `firstXWords` logs from 0 to x words.
 const firstXWords = (words, x) => words.slice(0,(words.indexOf(x)+1))
-console.log(firstXWords(words, "swack"))
+// console.log(firstXWords(words, "swack"))
 
 //  `subsetOfWords` logs from x to y words.
 const subsetOfWords = (words, x, y) => words.slice((words.indexOf(x)),(words.indexOf(y)+1))
-console.log(subsetOfWords(words, "feens", "junta"))
+// console.log(subsetOfWords(words, "feens", "junta"))
 
 //  `sortWords` logs all the words, sorted alphabetically.
 const sortWords = (words) => words.sort()
-console.log(sortWords(words))
+// console.log(sortWords(words))
