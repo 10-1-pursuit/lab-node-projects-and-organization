bf8a6d6c4b920b17de0f860eddbc7762ab49050a
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-05-27 22:04:06 -0400

    GotTenWords

--- index.js
+++ index.js
@@ -1,4 +1,65 @@
 
 
-const wordsJSON = require("./words.json")
-console.log(wordsJSON)
+const wordsJSON = require("./words.json");
+
+
+// - `allWords` logs all the words.
+// - `firstTenWords` logs the first ten words.
+// - `nextTenWords` logs the next 10 words.
+// - `firstXWords` logs from 0 to x words.
+// - `subsetOfWords` logs from x to y words.
+// - `sortWords` logs all the words, sorted alphabetically.
+
+// Write the following functions
+
+// - `wordsWithQ` returns all the words that contain the letter q
+// - `findWordsWithLetter` takes an argument `letter` and returns all the words with that matching letter
+
+// **Note:** When creating this functionality, it's okay to use just a sample set of 3-10 words. This step can make troubleshooting a bit easier. Be sure to test for the following scenarios:
+
+// - find a word with the matching letter
+// - find a word with more than one of the matching letter
+// - find a word with no matching letters
+
+// Instead of hard-coding the value for `findWordsWithLetter`, instead set it up so the user can input a letter from the terminal.
+
+// The following would test the letter P with the `findWordsWithLetter` function:
+
+// ```bash
+// node index.js p
+// ```
+
+// - Write a function `lettersMatch` that lets you input up to 5 letters in any order and returns the words that match the criteria (don't worry about solving for duplicate letters).
+//   - `node index.js aeiou`
+// - Write a function `letttersExactMatch` that lets you input a five-character string. Put letters with the exact placement in their place, and put a placeholder like `.` or `_` for unknown letters.
+// - `node index.js exactly a_ie_`
+// - Update the function `lettersMatch` to handle duplicate letters
+// - Create a new function that can handle both inputs
+//   - One input for matched letters in the wrong position
+//   - The second input for matched letters in the correct position
+//   - `node index.js al__a o`
+
+
+
+let allWords = wordsJSON;
+function consoleAllWords(wordsJSON) {
+
+    return allWords;
+
+}
+
+function consoleTenWords(allWords) {
+
+    let getTenWords = [];
+
+    for (let i = 0; i < 10; i++) {
+        // const getTenWords = allWords.some((onlyTen) => onlyTen.length === 10)
+        getTenWords.push(allWords[i])
+    }
+    return getTenWords;
+}
+console.log(consoleTenWords(allWords))
+
+
+// - `nextTenWords` logs the next 10 words.
+
