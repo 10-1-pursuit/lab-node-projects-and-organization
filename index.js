3581b7af61d0f688cbcd61c8b2cbeb536da3355e
christophersanchez <christophersanchez@pursuit.org>
2023-05-29 15:26:14 -0400

    Commented what needs to be placed as functions first...

---
+++ index.js
@@ -0,0 +1,21 @@
+const words = require('./words.json');
+
+// Function to log all the words
+allWords
+
+// Funciton to log the FIRST 🔟 words
+firstTenWords
+
+// Function to log the NEXT 🔟 words
+nextTenWords
+
+// Function to log from 0 - x words
+firstXWords
+
+// Function to log from x to y words 
+subsetOfWords
+
+// Function to log all the words, sorted alphabetically 🔤
+sortWords
+
+console.log(words);
