221f35c8702bc7d51de19a3c987e78ef147c7c84
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-05-30 09:52:40 -0400

    Just added a few more things

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const wordData = require("./words.json")
 
 const allWords = (wordData) => { console.log("All the words: ", wordData) };
-// allWords(wordData);
+allWords(wordData);
 
 const firstTenWords = (wordData) => {
     for (let i = 0; i < 10; i++) {
         console.log("The first ten words: ", wordData[i])
     };
 };
-// firstTenWords(wordData);
+firstTenWords(wordData);
 
 const nextTenWords = (wordData) => {
     for (let i = 10; i < 20; i++) {
         console.log("The next ten words: ", wordData[i])
     };
 };
-// nextTenWords(wordData);
+nextTenWords(wordData);
 
 const firstXWords = (wordData, xWords) => {
-    let x = xWords
+    let x = xWords;
     for (let i = 0; i < wordData.length && i < x; i++) {
-        console.log(wordData[i])
+        console.log(wordData[i]);
     };
 };
-// firstXWords(wordData, 15);
+firstXWords(wordData, 15);
 
 const subsetOfWords = (wordData, xWords, yWords) => {
     let x = xWords;
@@ -31,15 +31,15 @@ const subsetOfWords = (wordData, xWords, yWords) => {
     for (let i = x; i < wordData.length && i <= y; i++) {
         console.log(wordData[i]);
     };
-};
-// subsetOfWords(wordData, 5, 10);
+}
+subsetOfWords(wordData, 5, 10);
 
 const sortWords = (wordData) => {
     const sortedWords = wordData.sort();
     console.log(sortedWords);
 }
 
-// sortWords(wordData)
+sortWords(wordData);
 
 ///////////////////////////////////////////////////////
 
@@ -49,18 +49,11 @@ const wordsWithQ = (wordData) => {
             console.log("All words with 'q': ", wordData[i])
         };
     };
-};
-// wordsWithQ(wordData);
+}
+wordsWithQ(wordData);
 
 const findWordsWithLetter = (wordData, letter) => {
-    //     for (let i = 0; i < wordData.length; i++) {
-    //         if (wordData[i].includes(letter)) {
-    //             console.log("All words with the matching letter: ", wordData[i], i);
-    //         }
-    //     };
-    // }
     for (let i = 0; i < wordData.length; i++) {
-        // const theLetter = process.argv[2]
         if (wordData[i].includes(letter)) {
             console.log("Words with the matching letter: ", wordData[i]);
             var count = wordData[i].split(letter).length - 1;
@@ -68,14 +61,15 @@ const findWordsWithLetter = (wordData, letter) => {
                 console.log("Words with more then one letter: ", wordData[i]);
             }
         } else {
-            console.log("Words with no matching letters: ", wordData[i]);
+            console.log("Words with no matching letters: ", wordData[i]);;
         }
-    }
-};
-const letter = process.argv[2]
-findWordsWithLetter(wordData, letter)
+    };
+}
+const letter = process.argv[2];
+findWordsWithLetter(wordData, letter);
 
-
+const lettersMatch = () => { };
+lettersMatch();
 
 
 
