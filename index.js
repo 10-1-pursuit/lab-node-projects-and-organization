e345bde994e6861b6f9d2b38c071734d69a68800
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-05-30 01:10:00 -0400

    Just finished upgrading the functionality of the findWordsWithLetter function

--- index.js
+++ index.js
@@ -43,9 +43,37 @@ const sortWords = (wordData) => {
 
 ///////////////////////////////////////////////////////
 
+const wordsWithQ = (wordData) => {
+    for (let i = 0; i < wordData.length; i++) {
+        if (wordData[i].includes('q')) {
+            console.log("All words with 'q': ", wordData[i])
+        };
+    };
+};
+// wordsWithQ(wordData);
 
-
-
+const findWordsWithLetter = (wordData, letter) => {
+    //     for (let i = 0; i < wordData.length; i++) {
+    //         if (wordData[i].includes(letter)) {
+    //             console.log("All words with the matching letter: ", wordData[i], i);
+    //         }
+    //     };
+    // }
+    for (let i = 0; i < wordData.length; i++) {
+        // const theLetter = process.argv[2]
+        if (wordData[i].includes(letter)) {
+            console.log("Words with the matching letter: ", wordData[i]);
+            var count = wordData[i].split(letter).length - 1;
+            if (count > 1) {
+                console.log("Words with more then one letter: ", wordData[i]);
+            }
+        } else {
+            console.log("Words with no matching letters: ", wordData[i]);
+        }
+    }
+};
+const letter = process.argv[2]
+findWordsWithLetter(wordData, letter)
 
 
 
